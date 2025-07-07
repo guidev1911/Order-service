937f630773ffa80722ed0a837de53f4d22d5dfcd
guidev1911 <guilhermereal1911@gmail.com>
2025-07-06 23:41:59 -0300

    @Validated e exceções customizadas

--- order-service/src/main/java/com/microloja/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/microloja/order_service/controller/OrderController.java
@@ -6,12 +6,14 @@ import com.microloja.order_service.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
+@Validated
 public class OrderController {
 
     private final OrderService service;
